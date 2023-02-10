df07c4f52837d3925bcd4842889a416c08810bed
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-10 02:02:52 +0100

    Fix tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,21 @@
 from typing import TypeVar
 
-from rustpy import primitive
+from rustpy.primitive.bool_ import bool_
+from rustpy.primitive.i128 import i128
+from rustpy.primitive.i16 import i16
+from rustpy.primitive.i32 import i32
+from rustpy.primitive.i64 import i64
+from rustpy.primitive.i8 import i8
+from rustpy.primitive.isize import isize
+from rustpy.primitive.u128 import u128
+from rustpy.primitive.u16 import u16
+from rustpy.primitive.u32 import u32
+from rustpy.primitive.u64 import u64
+from rustpy.primitive.u8 import u8
+from rustpy.primitive.usize import usize
 
-Primitive = TypeVar('Primitive', primitive.bool_, primitive.i128,
-                    primitive.i16, primitive.i32, primitive.i64, primitive.i8,
-                    primitive.isize, primitive.u128, primitive.u16,
-                    primitive.u32, primitive.u64, primitive.u8,
-                    primitive.usize)
+Primitive = TypeVar('Primitive', bool_, i128, i16, i32, i64, i8, isize, u128,
+                    u16, u32, u64, u8, usize)
 
 
 def equivalence(first: bool, second: bool) -> bool:
