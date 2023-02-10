b939628240b049a5e0e437ab7e1db21627b0d184
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-10 02:11:50 +0100

    Add test for '__lt__' magic method of primitives

---
+++ tests/primitive_tests/test_lt.py
@@ -0,0 +1,48 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from rustpy.primitive.bool_ import bool_
+from tests.utils import (Primitive,
+                         equivalence,
+                         implication)
+from . import strategies
+
+
+@given(strategies.finite_primitives_pairs)
+def test_basic(pair: Tuple[Primitive, Primitive]) -> None:
+    first, second = pair
+
+    result = first < second
+
+    assert isinstance(result, bool_)
+
+
+@given(strategies.finite_primitives)
+def test_irreflexivity(primitive: Primitive) -> None:
+    assert not primitive < primitive
+
+
+@given(strategies.finite_primitives_pairs)
+def test_asymmetry(pair: Tuple[Primitive, Primitive]) -> None:
+    first, second = pair
+
+    assert implication(first < second, not second < first)
+
+
+@given(strategies.finite_primitives_triplets)
+def test_transitivity(triplet: Tuple[Primitive, Primitive, Primitive]) -> None:
+    first, second, third = triplet
+
+    assert implication(first < second < third, first < third)
+
+
+@given(strategies.finite_primitives_pairs)
+def test_equivalents(pair: Tuple[Primitive, Primitive]) -> None:
+    first, second = pair
+
+    result = first < second
+
+    assert equivalence(result, second > first)
+    assert equivalence(result, second >= first != second)
+    assert equivalence(result, first <= second != first)
