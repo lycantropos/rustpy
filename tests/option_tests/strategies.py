a4417f1cb577cf224d578e46c8d2545bbe825377
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-10 21:05:12 +0100

    Fix strategies

--- tests/option_tests/strategies.py
+++ tests/option_tests/strategies.py
@@ -5,14 +5,17 @@ from rustpy.option import None_, Some
 hashable_values = (_st.none() | _st.booleans() | _st.integers()
                    | _st.fractions() | _st.floats() | _st.complex_numbers()
                    | _st.binary() | _st.text() | _st.dates() | _st.datetimes())
-hashable_values |= _st.recursive(hashable_values, _st.frozensets)
+hashable_values |= _st.recursive(hashable_values, _st.frozensets,
+                                 max_leaves=5)
 values = (hashable_values | _st.sets(hashable_values)
           | _st.recursive(hashable_values,
-                          lambda strategy: _st.lists(strategy).map(tuple)))
+                          lambda strategy: _st.lists(strategy).map(tuple),
+                          max_leaves=5))
 deferred_values = _st.deferred(lambda: values)
 values |= _st.recursive(
         deferred_values,
-        lambda strategy: _st.lists(strategy) | _st.lists(strategy).map(tuple)
+        lambda strategy: _st.lists(strategy) | _st.lists(strategy).map(tuple),
+        max_leaves=5
 )
 values |= _st.dictionaries(hashable_values, deferred_values)
 empty_factories = _st.builds(lambda value: (lambda: value), values)
