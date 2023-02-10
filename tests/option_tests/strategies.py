43a3cda51408aaffee3f8452f9d0ae863b00b972
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-10 20:31:12 +0100

    Add test for 'Option.unwrap_or_else' method

--- tests/option_tests/strategies.py
+++ tests/option_tests/strategies.py
@@ -3,6 +3,7 @@ from hypothesis import strategies as _st
 from rustpy.option import None_, Some
 
 values = _st.from_type(object)
+empty_factories = _st.builds(lambda value: (lambda: value), values)
 nones = _st.builds(None_)
 somes = _st.builds(Some, values)
 options = nones | somes
---
+++ tests/option_tests/test_unwrap_or_else.py
@@ -0,0 +1,22 @@
+from typing import (Any,
+                    Callable)
+
+from hypothesis import given
+
+from rustpy.option import (None_,
+                           Some)
+from . import strategies
+
+
+@given(strategies.nones, strategies.values)
+def test_none(none: None_, value: Any) -> None:
+    result = none.unwrap_or_else(lambda: value)
+
+    assert result is value
+
+
+@given(strategies.somes, strategies.values)
+def test_some(some: Some, factory: Callable[[], Any]) -> None:
+    result = some.unwrap_or_else(factory)
+
+    assert result is some.unwrap()
