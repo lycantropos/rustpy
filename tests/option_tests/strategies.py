37b1c63041fc6d630e79722194f21b238fd50630
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-11 21:19:43 +0100

    Complete strategies

--- tests/option_tests/strategies.py
+++ tests/option_tests/strategies.py
@@ -5,6 +5,8 @@ from hypothesis import strategies as _st
 from rustpy.option import (None_,
                            Some)
 
+MAX_RECURSION_DEPTH = 3
+
 comparable_values_categories_tuple = (
     _st.booleans() | _st.integers() | _st.fractions()
     | _st.floats(allow_nan=False),
@@ -13,9 +15,20 @@ comparable_values_categories_tuple = (
     _st.dates(),
     _st.datetimes()
 )
-comparable_values = _st.one_of(comparable_values_categories_tuple)
-comparable_values_categories = _st.sampled_from(
-        comparable_values_categories_tuple
+comparable_values = _st.recursive(
+        _st.one_of(comparable_values_categories_tuple),
+        lambda values: _st.lists(values) | _st.lists(values).map(tuple),
+        max_leaves=MAX_RECURSION_DEPTH
+)
+comparable_values_categories = _st.recursive(
+        _st.sampled_from(comparable_values_categories_tuple),
+        lambda category:
+        (category.map(_st.lists)
+         | category.map(lambda values: _st.lists(values).map(tuple))),
+        max_leaves=MAX_RECURSION_DEPTH
+)
+comparable_values_categories |= _st.lists(comparable_values_categories).map(
+        lambda variants: _st.tuples(*variants)
 )
 hashable_equatable_values = (
         _st.none() | _st.sampled_from([Ellipsis, NotImplemented])
@@ -25,18 +38,18 @@ hashable_equatable_values = (
 )
 hashable_equatable_values |= _st.recursive(hashable_equatable_values,
                                            _st.frozensets,
-                                           max_leaves=5)
+                                           max_leaves=MAX_RECURSION_DEPTH)
 equatable_values = (
         hashable_equatable_values | _st.sets(hashable_equatable_values)
         | _st.recursive(hashable_equatable_values,
                         lambda strategy: _st.lists(strategy).map(tuple),
-                        max_leaves=5)
+                        max_leaves=MAX_RECURSION_DEPTH)
 )
 deferred_equatable_values = _st.deferred(lambda: equatable_values)
 equatable_values |= _st.recursive(
         deferred_equatable_values,
         lambda strategy: _st.lists(strategy) | _st.lists(strategy).map(tuple),
-        max_leaves=5
+        max_leaves=MAX_RECURSION_DEPTH
 )
 equatable_values |= _st.dictionaries(hashable_equatable_values,
                                      deferred_equatable_values)
@@ -63,7 +76,7 @@ comparable_options_pairs = comparable_options_categories.flatmap(
         lambda values: _st.tuples(values, values)
 )
 comparable_options_triplets = comparable_somes_categories.flatmap(
-    lambda values: _st.tuples(values, values, values)
+        lambda values: _st.tuples(values, values, values)
 )
 options_maps = _st.builds(lambda value: (lambda _: value), options)
 options_empty_factories = _st.builds(lambda value: (lambda: value), options)
