da624b7a51f8e5a550ad5a7f3a96999105a23c82
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-13 21:04:38 +0100

    Add test for 'sub' method of integer primitives

---
+++ tests/primitive_tests/test_sub.py
@@ -0,0 +1,32 @@
+from typing import Tuple
+
+import pytest
+from hypothesis import given
+
+from tests.utils import Integer
+from . import strategies
+
+
+@given(strategies.integers_pairs)
+def test_basic(pair: Tuple[Integer, Integer]) -> None:
+    first, second = pair
+
+    try:
+        result = first.sub(second)
+    except OverflowError:
+        pass
+    else:
+        assert isinstance(result, type(first))
+
+
+@given(strategies.integers_pairs)
+def test_connection_with___add__(pair: Tuple[Integer, Integer]) -> None:
+    first, second = pair
+
+    try:
+        result = first.sub(second)
+    except OverflowError:
+        with pytest.raises(OverflowError):
+            first - second
+    else:
+        assert result == first - second
