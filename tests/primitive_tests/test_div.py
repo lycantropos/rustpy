aa35b74e4bb4417ece36fac181cee291cc83392c
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-13 21:53:57 +0100

    Add test for 'div' method of integer primitives

---
+++ tests/primitive_tests/test_div.py
@@ -0,0 +1,32 @@
+from typing import Tuple
+
+import pytest
+from hypothesis import given
+
+from tests.utils import Integer
+from . import strategies
+
+
+@given(strategies.integers_pairs)
+def test_basic(pair: Tuple[Integer, Integer]) -> None:
+    first, second = pair
+
+    try:
+        result = first.div(second)
+    except (OverflowError, ZeroDivisionError):
+        pass
+    else:
+        assert isinstance(result, type(first))
+
+
+@given(strategies.integers_pairs)
+def test_connection_with___truediv__(pair: Tuple[Integer, Integer]) -> None:
+    first, second = pair
+
+    try:
+        result = first.div(second)
+    except (OverflowError, ZeroDivisionError) as error:
+        with pytest.raises(type(error)):
+            first / second
+    else:
+        assert result == first / second
