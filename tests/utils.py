0c384c261b8c4c6200e6f46945d117ebbbb0387f
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-13 02:39:39 +0100

    Fix tests

--- tests/utils.py
+++ tests/utils.py
@@ -5,6 +5,11 @@ import pytest
 
 from rustpy import primitive
 
+Integer = _t.TypeVar('Integer', primitive.i128, primitive.i16,
+                     primitive.i32, primitive.i64, primitive.i8,
+                     primitive.isize, primitive.u128, primitive.u16,
+                     primitive.u32, primitive.u64, primitive.u8,
+                     primitive.usize)
 Primitive = _t.TypeVar('Primitive', primitive.bool_, primitive.f32,
                        primitive.f64, primitive.i128, primitive.i16,
                        primitive.i32, primitive.i64, primitive.i8,
