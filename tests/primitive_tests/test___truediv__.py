2ddf7b86f2ec1dfd7cf72820ab38ab7ceb21f779
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-13 21:41:58 +0100

    Add test for '__truediv__' magic method of integer primitives

---
+++ tests/primitive_tests/test___truediv__.py
@@ -0,0 +1,40 @@
+from typing import Tuple
+
+import pytest
+from hypothesis import given
+
+from tests.utils import Integer
+from . import strategies
+
+
+@given(strategies.divisible_integers_pairs)
+def test_basic(pair: Tuple[Integer, Integer]) -> None:
+    dividend, divisor = pair
+
+    try:
+        result = dividend / divisor
+    except OverflowError:
+        pass
+    else:
+        assert isinstance(result, type(dividend))
+
+
+@given(strategies.divisible_integers_pairs)
+def test_connection_with_mod(pair: Tuple[Integer, Integer]) -> None:
+    dividend, divisor = pair
+
+    try:
+        result = dividend / divisor
+    except OverflowError:
+        with pytest.raises(OverflowError):
+            dividend % divisor
+    else:
+        assert result * divisor + dividend % divisor == dividend
+
+
+@given(strategies.integers_with_zeros)
+def test_zero_divisor(integer_with_zero: Tuple[Integer, Integer]) -> None:
+    integer, zero = integer_with_zero
+
+    with pytest.raises(ZeroDivisionError):
+        integer / zero
