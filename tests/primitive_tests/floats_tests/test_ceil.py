67765483cdf957e578ca0992762c6abddfe47d05
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-16 21:36:17 +0100

    Add test for 'ceil' method of floating point primitives

---
+++ tests/primitive_tests/floats_tests/test_ceil.py
@@ -0,0 +1,31 @@
+from hypothesis import given
+
+from tests.utils import Float
+from . import strategies
+
+
+@given(strategies.finite_floats)
+def test_basic(float_: Float) -> None:
+    result = float_.ceil()
+
+    assert isinstance(result, type(float_))
+
+
+@given(strategies.finite_floats_with_ones)
+def test_value(float_with_one: Float) -> None:
+    float_, one = float_with_one
+
+    result = float_.ceil()
+
+    assert float_ <= result <= float_ + one
+    assert result.fract() == type(float_)(0.0)
+
+
+@given(strategies.finite_floats_with_ones)
+def test_alternatives(float_with_one: Float) -> None:
+    float_, one = float_with_one
+
+    result = float_.ceil()
+
+    assert result == -(-float_).floor()
+    assert result == -(-float_).div_euclid(one)
