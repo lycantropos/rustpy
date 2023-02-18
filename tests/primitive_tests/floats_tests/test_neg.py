5dfeacef768292e1698a17a1d17951af8a85a709
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-18 17:07:41 +0100

    Add test for 'neg' method of floating point primitives

---
+++ tests/primitive_tests/floats_tests/test_neg.py
@@ -0,0 +1,18 @@
+from hypothesis import given
+
+from tests.utils import Float
+from . import strategies
+
+
+@given(strategies.floats)
+def test_basic(float_: Float) -> None:
+    result = float_.neg()
+
+    assert isinstance(result, type(float_))
+
+
+@given(strategies.floats)
+def test_connection_with___neg__(float_: Float) -> None:
+    result = float_.neg()
+
+    assert result == -float_
