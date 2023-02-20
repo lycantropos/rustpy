0b040226ee2e283d8ec32ebaeef81d22a17ceb54
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-20 23:09:03 +0100

    Add test for '__lshift__' magic method of integer primitives

---
+++ tests/primitive_tests/integers_tests/test___lshift__.py
@@ -0,0 +1,61 @@
+from contextlib import suppress
+from typing import Tuple
+
+from hypothesis import given
+
+from rustpy.primitive import u32
+from tests.utils import Integer
+from . import strategies
+
+
+@given(strategies.integers, strategies.u32s)
+def test_basic(base: Integer, shift: u32) -> None:
+    with suppress(OverflowError):
+        result = base << shift
+
+        assert isinstance(result, type(base))
+
+
+@given(strategies.zero_integers, strategies.u32s)
+def test_left_absorbing_element(zero: Integer, shift: u32) -> None:
+    result = zero << shift
+
+    assert result == zero
+
+
+@given(strategies.integers, strategies.u32_zeros)
+def test_right_neutral_element(base: Integer, zero: u32) -> None:
+    result = base << zero
+
+    assert result == base
+
+
+@given(strategies.integers_pairs, strategies.u32s)
+def test___add___base(bases_pair: Tuple[Integer, Integer], shift: u32) -> None:
+    first_base, second_base = bases_pair
+
+    with suppress(OverflowError):
+        result = (first_base + second_base) << shift
+
+        assert result == (first_base << shift) + (second_base << shift)
+
+
+@given(strategies.integers, strategies.u32s, strategies.u32s)
+def test___add___shift(base: Integer,
+                       first_shift: u32,
+                       second_shift: u32) -> None:
+    with suppress(OverflowError):
+        result = base << (first_shift + second_shift)
+
+        assert result == (base << first_shift) << second_shift
+
+
+@given(strategies.integers_pairs, strategies.u32s)
+def test___mul___base(bases_pair: Tuple[Integer, Integer], shift: u32) -> None:
+    first_base, second_base = bases_pair
+
+    with suppress(OverflowError):
+        result = (first_base * second_base) << shift
+
+        assert result == (first_base << shift) * second_base
+        assert result == first_base * (second_base << shift)
