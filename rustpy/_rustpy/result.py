e4dcbca0782c088c4b972247a1049a478cf0fc5c
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-22 02:54:00 +0100

    Complete 'Result' protocol

--- rustpy/_rustpy/result.py
+++ rustpy/_rustpy/result.py
@@ -83,6 +83,9 @@ class Err(_t.Generic[_E]):
     def unwrap_or_else(self, _function: _t.Callable[[_E], _T]) -> _T:
         return _function(self._value)
 
+    __module__ = 'rustpy.result'
+    __slots__ = '_value',
+
     def __init__(self, value: _E) -> None:
         self._value = value
 
@@ -223,6 +226,9 @@ class Ok(_t.Generic[_T]):
 
     def unwrap_or_else(self, _function: _t.Callable[[_E], _T]) -> _T:
         return self._value
+
+    __module__ = 'rustpy.result'
+    __slots__ = '_value'
 
     def __init__(self, value: _T) -> None:
         self._value = value
