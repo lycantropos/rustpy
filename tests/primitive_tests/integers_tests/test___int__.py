2e95309361ba408ca2c2f498b6855a5fa1997b58
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-22 02:03:21 +0100

    Add test for '__int__' magic method of integer primitives

---
+++ tests/primitive_tests/integers_tests/test___int__.py
@@ -0,0 +1,25 @@
+from typing import (Tuple,
+                    Type)
+
+from hypothesis import given
+
+from tests.utils import Integer
+from . import strategies
+
+
+@given(strategies.signed_integers)
+def test_basic(integer: Integer) -> None:
+    result = int(integer)
+
+    assert isinstance(result, int)
+
+
+@given(strategies.integer_types_with_values)
+def test_round_trip(
+        integer_type_with_value: Tuple[Type[Integer], Integer]
+) -> None:
+    integer_type, integer = integer_type_with_value
+
+    result = int(integer)
+
+    assert integer_type(result) == integer
