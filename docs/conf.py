4210ce2b1e24b962321bf7ce4f8f78a5a2eec3ee
Azat Ibrakov <azatibrakov@gmail.com>
2023-02-24 00:36:16 +0100

    Fix docs configurations

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,11 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
 from datetime import date
+
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 import rustpy
