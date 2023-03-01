ee0f313ee3de8284c7405cbeba876d56fba8a9d8
Azat Ibrakov <azatibrakov@gmail.com>
2023-03-01 02:50:40 +0100

    Add test for '__xor__' magic method of integer primitives

---
+++ tests/primitive_tests/integers_tests/test___xor__.py
@@ -0,0 +1,40 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from tests.utils import Integer
+from . import strategies
+
+
+@given(strategies.integers_pairs)
+def test_basic(pair: Tuple[Integer, Integer]) -> None:
+    first, second = pair
+
+    result = first ^ second
+
+    assert isinstance(result, type(first))
+
+
+@given(strategies.integers_pairs)
+def test_commutativity(pair: Tuple[Integer, Integer]) -> None:
+    first, second = pair
+
+    result = first ^ second
+
+    assert result == second ^ first
+
+
+@given(strategies.integers_triplets)
+def test_associativity(triplet: Tuple[Integer, Integer, Integer]) -> None:
+    first, second, third = triplet
+
+    result = (first ^ second) ^ third
+
+    assert result == first ^ (second ^ third)
+
+
+@given(strategies.integers_with_zeros)
+def test_neutral_element(integer_with_zero: Tuple[Integer, Integer]) -> None:
+    integer, zero = integer_with_zero
+
+    assert integer ^ zero == integer == zero ^ integer
